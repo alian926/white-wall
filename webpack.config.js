5c0d89b34bdceafe21f7fe0f152f7a470096efa7
Alian <luantao@mininglamp.com>
2021-11-09 17:18:12 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// 将 JS 字符串生成为 style 节点
+					'style-loader',
+					// 将 CSS 转化成 CommonJS 模块
+					'css-loader',
+					// 将 Sass 编译成 CSS
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  target: 'web',
+	mode: 'production',
+};
